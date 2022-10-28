e0d27b97d7549dbacd583ffb5de85f59bd95648a
Tom Birdsong <tom.birdsong@kitware.com>
2022-10-28 15:18:30 -0400

    BUG: Update test image buffer in `itkCLEAddImageTestPython.py`
    Port missed behavior from cxx test where `itk::CLEImage::Update`
    must be manually called on filter output to force the CPU buffer
    to be updated.

--- wrapping/test/itkCLEAddImageTestPython.py
+++ wrapping/test/itkCLEAddImageTestPython.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+#==========================================================================
+#
+#   Copyright NumFOCUS
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          https://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#==========================================================================*/
+
 import itertools
 
 import itk
@@ -33,6 +51,8 @@ add_filter.SetInput2(image2)
 add_filter.Update()
 
 output_image = add_filter.GetOutput()
+output_image.Update()
+
 assert(itk.size(output_image) == [width, height])
 assert(itk.spacing(output_image) == [1,1])
 assert(itk.origin(output_image) == [0,0])
